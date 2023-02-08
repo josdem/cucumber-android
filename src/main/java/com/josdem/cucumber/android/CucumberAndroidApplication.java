0441accdbe305d63b9528f18c20fbbeb99f558a1
josdem <joseluis.delacruz@gmail.com>
2023-02-08 16:04:48 -0600

    Updating package names

--- src/main/java/com/josdem/cucumber/android/CucumberAndroidApplication.java
+++ src/main/java/com/josdem/cucumber/android/CucumberAndroidApplication.java
@@ -1,13 +1,13 @@
-package com.example.cucumberandroid;
+package com.josdem.cucumber.android;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class CucumberAndroidApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(CucumberAndroidApplication.class, args);
 	}
 
 }
--- src/test/java/com/josdem/cucumber/android/DemoApplicationTests.java
+++ src/test/java/com/josdem/cucumber/android/DemoApplicationTests.java
@@ -1,4 +1,4 @@
-package com.example.cucumberandroid;
+package com.josdem.cucumber.android;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
